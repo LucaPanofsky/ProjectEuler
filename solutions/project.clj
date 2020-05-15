c7ddb4de401df1678f94b6f32293b27c27ecc7b2
LucaPanofsky <luca.ferrari.sdc@gmail.com>
2020-05-15 22:41:52 +0200

    solutions project

---
+++ solutions/project.clj
@@ -0,0 +1,8 @@
+(defproject solutions "0.1.0-SNAPSHOT"
+  :description "Problems taken from https://projecteuler.net/"
+  :url "https://github.com/LucaPanofsky"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+ :dependencies [[org.clojure/clojure "1.10.0"]
+                [proto-repl "0.3.1"]
+                [org.clojure/tools.namespace "0.3.1"]])
