88aad231bf3e731c4c9d688f736a763b1aa44239
LucaPanofsky <luca.ferrari.sdc@gmail.com>
2020-05-15 23:01:55 +0200

    Problem1

--- solutions/src/solutions/problem1.clj
+++ solutions/src/solutions/problem1.clj
@@ -1,1 +1,12 @@
 (ns solutions.problem1)
+(use 'clojure.repl)
+
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+
+(defn  sum-of-multiples [integer-list]
+  (let [mult-of-3? (fn [n] (= 0 (rem n 3)))
+        mult-of-5? (fn [n] (= 0 (rem n 5)))]
+    (->> integer-list
+         (filter (fn [n] (or (mult-of-3? n) (mult-of-5? n))))
+         (reduce +))))
+    
